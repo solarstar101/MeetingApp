d2aa35dc471473895399dff107176d2d6408b8a9
solarstar101 <mmerino1@uth.tmc.edu>
2020-01-09 11:12:50 -0600

    completed basic setup of Home

---
+++ src/Components/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import styles from "./Styling/Home.module.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-10 col-md-10 col-lg-8 col-xl-7">
+            <div
+              className={`display-4 text-primary mt-3 mb-2  ${styles.biggerLead}`}
+            >
+              Meeting Log
+            </div>
+            <p class="lead">
+              This simple app creates meetings, allows people to check in, and
+              picks random users to award giveaways. It's a good example of a
+              Single Page Application which includes connection to a database
+              and routing. It's a practical way to learn{" "}
+              <a href="https://reactjs.org/">React</a>
+              with <a href="https://firebase.google.com">Firebase</a>.
+            </p>
+
+            <a href="/register" class="btn btn-outline-primary mr-2">
+              Register
+            </a>
+            <a href="/login" class="btn btn-outline-primary mr-2">
+              Log In
+            </a>
+            <a href="/meetings" class="btn btn-primary">
+              Meetings
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
